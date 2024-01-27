1915355e76a2e68effb6eb47de8c4d1d7aa63261
Albert Barsegyan <albert.barseghyan@banzai.io>
2024-01-27 21:29:18 +0400

    fix: remove odd configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { RouterProvider } from "react-router-dom";
 import { unauthenticatedRoutes } from "./routes/unauthenticated-routes.tsx";
-import { useScreenSize } from "./hooks/use-screen-size.tsx";
-import { BreakPointSize } from "./constants/breakpoints-constants.ts";
+// import { useScreenSize } from "./hooks/use-screen-size.tsx";
+// import { BreakPointSize } from "./constants/breakpoints-constants.ts";
 import { authenticatedRoutes } from "./routes/authenticated-routes.tsx";
 import { useAuth } from "./hooks/use-auth.tsx";
-import { Stack, Typography } from "@mui/material";
+// import { Stack, Typography } from "@mui/material";
 
 function App() {
-  const screenWidth = useScreenSize();
+  // const screenWidth = useScreenSize();
   const { isAuthenticated } = useAuth();
 
-  if (screenWidth <= BreakPointSize.Mobile) {
-    return (
-      <RouterProvider
-        router={isAuthenticated ? authenticatedRoutes : unauthenticatedRoutes}
-      />
-    );
-  }
-
+  // if (screenWidth <= BreakPointSize.Mobile) {
   return (
-    <Stack sx={{ height: "100vh" }} alignItems="center" justifyContent="center">
-      <Typography
-        textAlign="center"
-        sx={{ fontSize: "48px", color: "lightColor" }}
-      >
-        Please use mobile device <br /> 😇 😇 😇
-      </Typography>
-    </Stack>
+    <RouterProvider
+      router={isAuthenticated ? authenticatedRoutes : unauthenticatedRoutes}
+    />
   );
+  // }
+
+  // return (
+  //   <Stack sx={{ height: "100vh" }} alignItems="center" justifyContent="center">
+  //     <Typography
+  //       textAlign="center"
+  //       sx={{ fontSize: "48px", color: "lightColor" }}
+  //     >
+  //       Please use mobile device <br /> 😇 😇 😇
+  //     </Typography>
+  //   </Stack>
+  // );
 }
 
 export default App;
