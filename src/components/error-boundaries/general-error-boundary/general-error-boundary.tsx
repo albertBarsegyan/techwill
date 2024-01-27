1139d80111feacb7aa48d2ed0b56c6b91f144104
Albert Barsegyan <albert.barseghyan@banzai.io>
2024-01-27 20:23:43 +0400

    fix: issue

--- src/components/error-boundaries/general-error-boundary/general-error-boundary.tsx
+++ src/components/error-boundaries/general-error-boundary/general-error-boundary.tsx
@@ -1,9 +1,5 @@
 import { Component, ErrorInfo } from "react";
 import { ComponentWithChildren } from "../../../types/component-types.ts";
-import {
-  ProjectCurrentEnvironment,
-  ProjectEnvironmentVariant,
-} from "../../../constants/evironment-constants.ts";
 
 export class GeneralErrorBoundary extends Component<ComponentWithChildren> {
   state = { hasError: false };
@@ -13,8 +9,7 @@ export class GeneralErrorBoundary extends Component<ComponentWithChildren> {
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    if (ProjectCurrentEnvironment === ProjectEnvironmentVariant.Development)
-      console.log({ error, errorInfo });
+    console.log({ error, errorInfo });
   }
 
   render() {
--- src/constants/evironment-constants.ts
+++
@@ -1,6 +0,0 @@
-export const enum ProjectEnvironmentVariant {
-  Production = "Production ",
-  Development = "Development",
-}
-
-export const ProjectCurrentEnvironment = import.meta.env.VITE_ENVIRONMEN;
