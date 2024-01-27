e70f8948e97ecaaec7f7355635fbbf23138f6524
Albert Barsegyan <albert.barseghyan@banzai.io>
2024-01-28 01:52:26 +0400

    fix: remove overflow

--- src/layouts/page-layout/page-layout.tsx
+++ src/layouts/page-layout/page-layout.tsx
@@ -2,9 +2,5 @@ import { Box } from "@mui/material";
 import { PropsWithChildren } from "react";
 
 export function PageLayout({ children }: PropsWithChildren) {
-  return (
-    <Box sx={{ padding: "0 20px", height: "100vh", overflow: "hidden" }}>
-      {children}
-    </Box>
-  );
+  return <Box sx={{ padding: "0 20px", height: "100vh" }}>{children}</Box>;
 }
